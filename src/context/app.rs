0e6c40475ee091faa44039aa4d8b558241149876
Jens Krause <email@jkrause.io>
2023-06-17 15:23:50 +0200

    Render qr code + re-organize component structure

--- src/context/app.rs
+++ src/context/app.rs
@@ -8,7 +8,6 @@ pub(crate) struct AppContext {
 pub fn provide_app_context(cx: Scope) {
   let connect_str =
     std::env::var("FM_CONNECT_STRING").expect("FM_CONNECT_STRING environment variable not set");
-
   let context = AppContext { connect_str };
 
   provide_context(cx, context);
