71a24ecbe3f075027962731563c8fc368fa00de1
Jens Krause <email@jkrause.io>
2023-06-17 15:27:57 +0200

    Read env

--- src/context/app.rs
+++ src/context/app.rs
@@ -6,10 +6,9 @@ pub(crate) struct AppContext {
 }
 
 pub fn provide_app_context(cx: Scope) {
-  // let connect_str =
-  //   std::env::var("FM_CONNECT_STRING").expect("FM_CONNECT_STRING environment variable not set");
+  let connect_str =
+    std::env::var("FM_CONNECT_STRING").expect("FM_CONNECT_STRING environment variable not set");
 
-  let connect_str = "fed1152z30ck6zctk009cn3phhktr9hjja0r8j84m7jxyglt8hvfa775tska8hw794htza7rgt6c8z9ggsqq4waen5te0xyerwt3s9cczuvf6xyurzde5974fny3frtvns4s0wvtuqszjs9h".to_string();
   let context = AppContext { connect_str };
 
   provide_context(cx, context);
